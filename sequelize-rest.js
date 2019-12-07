d76304210b3c52a3603a59b7af4cf4f47483ff9a
Svdbroek <s_in_the_water@hotmail.com>
2019-12-07 14:13:48 +0100

    added all endpoints and error handling

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -7,15 +7,18 @@ const databaseUrl =
 const db = new Sequelize(databaseUrl);
 
 // syncing the db
-db.sync()
+db.sync({ alter: true })
   .then(console.log("database connected"))
+  .then(createMovies)
   .catch(console.error);
 
 // defining a model
 
 const Movie = db.define("movie", {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    unique: true
+    //using unique so the database doesn't fill up wit copies of the dummy data on server reset, not sure how to handle the error
   },
   yearOfRealease: {
     type: Sequelize.INTEGER
@@ -24,29 +27,73 @@ const Movie = db.define("movie", {
     type: Sequelize.STRING
   }
 });
-// creating some movies
-Movie.create({
-  title: "Star Wars: Episode I - the Phantom Menace",
-  yearOfRealease: 1999,
-  synopsys: "Turmoil has engulfed the Galactic Republic..."
-});
-Movie.create({
-  title: "Paprika",
-  yearOfRealease: 2006,
-  synopsys:
-    "Paprika, a therapist, must save the day when a machine that allows therapists to enter the dreams of their patients is stolen"
-});
+//creating some data
+function createMovies() {
+  Movie.create({
+    title: "Star Wars: Episode I - the Phantom Menace",
+    yearOfRealease: 1999,
+    synopsys: "Turmoil has engulfed the Galactic Republic..."
+  });
+  Movie.create({
+    title: "Paprika",
+    yearOfRealease: 2006,
+    synopsys:
+      "Paprika, a therapist, must save the day when a machine that allows therapists to enter the dreams of their patients is stolen"
+  });
 
-Movie.create({
-  title: "Hoodwinked",
-  yearOfRealease: 2005,
-  synopsys:
-    "Grizzly and Stork are critter cops who are investigating a disturbance of the peace between Granny, her granddaughter Little Red Riding Hood, a Big Bad Wolf and a Woodsman..."
-});
-
+  Movie.create({
+    title: "Hoodwinked",
+    yearOfRealease: 2005,
+    synopsys:
+      "Grizzly and Stork are critter cops who are investigating a disturbance of the peace between Granny, her granddaughter Little Red Riding Hood, a Big Bad Wolf and a Woodsman..."
+  });
+}
 // creating the express app server
 
 const app = express();
 const port = process.env.PORT || 4000;
+const bodyParser = require("body-parser");
+const parserMiddleware = bodyParser.json();
 
-app.listen(port, console.log(`listening on requests on port ${port}`));
+//some middleware
+
+app.use(parserMiddleware);
+//endpoints
+
+app.post("/movie", (req, res, nxt) => {
+  Movie.create(req.body)
+    .then(object => res.send(object))
+    .catch(err => nxt(err));
+});
+
+app.get("/movie", (req, res, nxt) => {
+  Movie.findAll()
+    .then(info => res.send(info))
+    .catch(err => nxt(err));
+});
+
+app.get("/movie/:id", (req, res, nxt) => {
+  Movie.findByPk(req.params.id)
+    .then(info => res.send(info))
+    .catch(err => nxt(err));
+});
+
+app.put("/movie/:id", (req, res, nxt) => {
+  Movie.findByPk(req.params.id)
+    .then(movie => {
+      if (movie) {
+        movie.update(req.body).then(movie => res.send(movie));
+      } else {
+        nxt();
+      }
+    })
+    .catch(err => nxt(err));
+});
+
+app.delete("/movie/:id", (req, res, nxt) => {
+  Movie.findByPk(req.params.id)
+    .then(movie => movie.destroy())
+    .catch(err => nxt(err));
+});
+
+app.listen(port, console.log(`listening for requests on port ${port}`));
