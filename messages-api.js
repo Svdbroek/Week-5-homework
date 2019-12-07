8b8d4f10e6cf4f988ff2b908d5cb8f6ac6a367b9
Svdbroek <s_in_the_water@hotmail.com>
2019-12-07 11:53:39 +0100

    server responds to text messages

--- messages-api.js
+++ messages-api.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const parserMiddleware = bodyParser.json();
 const app = express();
 const port = 3000;
+
+app.use(parserMiddleware);
+
+app.post("/message", (req, res, nxt) => {
+  console.log(req.body.text),
+    res.send({ message: "Message received loud and clear" });
+});
 
 app.listen(port, console.log(`listening on ${port}`));
