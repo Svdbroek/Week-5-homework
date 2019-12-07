7b85b6eac0abc7445ad3a8784bef0da968874927
Svdbroek <s_in_the_water@hotmail.com>
2019-12-07 11:59:30 +0100

    server now gives correct error when no valid body is present

--- messages-api.js
+++ messages-api.js
@@ -7,8 +7,12 @@ const port = 3000;
 app.use(parserMiddleware);
 
 app.post("/message", (req, res, nxt) => {
-  console.log(req.body.text),
-    res.send({ message: "Message received loud and clear" });
+  if (req.body.text) {
+    console.log(req.body.text),
+      res.send({ message: "Message received loud and clear" });
+  } else {
+    res.status(400).send("Bad request");
+  }
 });
 
 app.listen(port, console.log(`listening on ${port}`));
